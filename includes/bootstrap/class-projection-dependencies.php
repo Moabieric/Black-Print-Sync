b3a3e38a3ab9dad050435f4e30b27527395d8f18
Moabieric <181367258+Moabieric@users.noreply.github.com>
2026-09-02 11:24:02 +0200

    class projection cleanup

--- includes/bootstrap/class-projection-dependencies.php
+++ includes/bootstrap/class-projection-dependencies.php
@@ -27,36 +27,3 @@ require_once BP_COMMERCE_PATH
     . 'projection/woocommerce/WooCommerceProjectionExecutor.php';
 
 
-/*
-|--------------------------------------------------------------------------
-| DTO
-|--------------------------------------------------------------------------
-*/
-
-require_once BP_COMMERCE_PATH
-    . 'projection/dto/ProjectionResult.php';
-
- /*
-|--------------------------------------------------------------------------
-| Contracts
-|--------------------------------------------------------------------------
-*/
-
-require_once BP_COMMERCE_PATH
-    . 'projection/contracts/ProductProjectorInterface.php';
-
-require_once BP_COMMERCE_PATH
-    . 'projection/contracts/ProjectionExecutorInterface.php';
-
-
-/*
-|--------------------------------------------------------------------------
-| WooCommerce Projection
-|--------------------------------------------------------------------------
-*/
-
-require_once BP_COMMERCE_PATH
-    . 'projection/woocommerce/WooCommerceProductProjector.php';
-
-require_once BP_COMMERCE_PATH
-    . 'projection/woocommerce/WooCommerceProjectionExecutor.php';
